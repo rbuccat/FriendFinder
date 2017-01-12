2873243e3ce0b01f185bb7c14a95b3c056b6082e
rbuccat <richbuccat@yahoo.com>
2017-01-11 18:21:45 -0800

    homework 13 not finished, turning in what I have

---
+++ app/routing/api-routes.js
@@ -0,0 +1,20 @@
+var friendsData = require("../data/friends.js");
+
+module.exports = function(app) {
+
+	app.get("/api/friends", function(req, res) {
+    	res.json(friendsData);
+  	});
+
+	app.post("/api/friends", function(req, res) {
+		friendsData.push(req.body);
+	});
+
+	app.post("/api/clear", function() {
+    friendsData = [];
+
+    console.log(friendsData);
+  });
+
+};
+
